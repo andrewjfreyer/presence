a7468ef161c06192197ac52daaefb2982ab807e8
Andrew J Freyer <andrew.freyer@gmail.com>
2018-02-09 20:20:07 -0700

    Bug correction

--- beacon.sh
+++ beacon.sh
@@ -8,52 +8,51 @@
 MQTT_CONFIG=mqtt_preferences ; [ -f $MQTT_CONFIG ] && source $MQTT_CONFIG
 
 if [[ $1 == "parse" ]]; then
-  packet=""
-  capturing=""
-  count=0
-  while read line
-  do
-    count=$[count + 1]
-    if [ "$capturing" ]; then
-      if [[ $line =~ ^[0-9a-fA-F]{2}\ [0-9a-fA-F] ]]; then
-        packet="$packet $line"
-      else
-        if [[ $packet =~ ^04\ 3E\ 2A\ 02\ 01\ .{26}\ 02\ 01\ .{14}\ 02\ 15 ]]; then
-          UUID=`echo $packet | sed 's/^.\{69\}\(.\{47\}\).*$/\1/'`
-          MAJOR=`echo $packet | sed 's/^.\{117\}\(.\{5\}\).*$/\1/'`
-          MINOR=`echo $packet | sed 's/^.\{123\}\(.\{5\}\).*$/\1/'`
-          POWER=`echo $packet | sed 's/^.\{129\}\(.\{2\}\).*$/\1/'`
-          UUID=`echo $UUID | sed -e 's/\ //g' -e 's/^\(.\{8\}\)\(.\{4\}\)\(.\{4\}\)\(.\{4\}\)\(.\{12\}\)$/\1-\2-\3-\4-\5/'`
-          MAJOR=`echo $MAJOR | sed 's/\ //g'`
-          MAJOR=`echo "ibase=16; $MAJOR" | bc`
-          MINOR=`echo $MINOR | sed 's/\ //g'`
-          MINOR=`echo "ibase=16; $MINOR" | bc`
-          POWER=`echo "ibase=16; $POWER" | bc`
-          POWER=$[POWER - 256]
-    
-          #compare UUID to database
-          JSON_MSG="{\"confidence\":\"100\",\"name\":\"\",\"uuid\":\"$UUID\",\"major\":\"$MAJOR\",\"minor\":\"$MINOR\",\"power\":\"$POWER\"}"
+	packet=""
+	capturing=""
+	count=0
+	while read line
+	do
+		count=$[count + 1]
+		if [ "$capturing" ]; then
+			if [[ $line =~ ^[0-9a-fA-F]{2}\ [0-9a-fA-F] ]]; then
+				packet="$packet $line"
+			else
+				if [[ $packet =~ ^04\ 3E\ 2A\ 02\ 01\ .{26}\ 02\ 01\ .{14}\ 02\ 15 ]]; then
+					UUID=`echo $packet | sed 's/^.\{69\}\(.\{47\}\).*$/\1/'`
+					MAJOR=`echo $packet | sed 's/^.\{117\}\(.\{5\}\).*$/\1/'`
+					MINOR=`echo $packet | sed 's/^.\{123\}\(.\{5\}\).*$/\1/'`
+					POWER=`echo $packet | sed 's/^.\{129\}\(.\{2\}\).*$/\1/'`
+					UUID=`echo $UUID | sed -e 's/\ //g' -e 's/^\(.\{8\}\)\(.\{4\}\)\(.\{4\}\)\(.\{4\}\)\(.\{12\}\)$/\1-\2-\3-\4-\5/'`
+					MAJOR=`echo $MAJOR | sed 's/\ //g'`
+					MAJOR=`echo "ibase=16; $MAJOR" | bc`
+					MINOR=`echo $MINOR | sed 's/\ //g'`
+					MINOR=`echo "ibase=16; $MINOR" | bc`
+					POWER=`echo "ibase=16; $POWER" | bc`
+					POWER=$[POWER - 256]
+		
+					#compare UUID to database
+					JSON_MSG="{\"confidence\":\"100\",\"name\":\"\",\"uuid\":\"$UUID\",\"major\":\"$MAJOR\",\"minor\":\"$MINOR\",\"power\":\"$POWER\"}"
 
-          #send message via MQTT of beacon
-          /usr/local/bin/mosquitto_pub -h "$mqtt_address" -u "$mqtt_user" -P "$mqtt_password" -t "$mqtt_topicpath$1" -m "$JSON_MSG"
+					#send message via MQTT of beacon
+					/usr/local/bin/mosquitto_pub -h "$mqtt_address" -u "$mqtt_user" -P "$mqtt_password" -t "$mqtt_topicpath$1" -m "$JSON_MSG"
 
-          fi
-        fi
-        capturing=""
-        packet=""
-      fi
-    fi
+				fi
+				capturing=""
+				packet=""
+			fi
+		fi
 
-    if [ ! "$capturing" ]; then
-      if [[ $line =~ ^\> ]]; then
-        packet=`echo $line | sed 's/^>.\(.*$\)/\1/'`
-        capturing=1
-      fi
-    fi
-  done
+		if [ ! "$capturing" ]; then
+			if [[ $line =~ ^\> ]]; then
+				packet=`echo $line | sed 's/^>.\(.*$\)/\1/'`
+				capturing=1
+			fi
+		fi
+	done
 else
-  sudo hcitool lescan --duplicates 1>/dev/null &
-  if [ "$(pidof hcitool)" ]; then
-    sudo hcidump --raw | ./$0 parse $1
-  fi
+	sudo hcitool lescan --duplicates 1>/dev/null &
+	if [ "$(pidof hcitool)" ]; then
+		sudo hcidump --raw | ./$0 parse $1
+	fi
 fi
