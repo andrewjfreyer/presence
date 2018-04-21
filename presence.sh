089f117a792ab9644f65a3bf737d6077d4a54b86
Andrew J Freyer <andrew.freyer@gmail.com>
2018-04-21 16:46:56 -0600

    0.4.11
    Debug message updates

--- presence.sh
+++ presence.sh
@@ -17,7 +17,7 @@
 # ----------------------------------------------------------------------------------------
 
 #version number
-Version=0.4.10
+Version=0.4.11
 
 #color output 
 ORANGE='\033[0;33m'
@@ -299,10 +299,10 @@ numberOfGuests=$((${#macaddress_guests[@]}))
 #startup message
 echo -e "${GREEN}presence $Version ${NC} - Started. Performance predictions based on current settings:"
 #all owners at home 
-echo -e "  > Est. to recognize all ($numberOfOwners) owners as 'away' from 'home': $(( numberOfOwners * (2 * verifyByRepeatedlyQuerying + verificationLoopDelay * verifyByRepeatedlyQuerying) + (beaconScanEnabled == 1 ? beaconScanInterval : 0 ))) seconds to $(( delayBetweenOwnerScansWhenPresent + numberOfOwners * nameScanTimeout * verificationLoopDelay * verifyByRepeatedlyQuerying + (beaconScanEnabled == 1 ? beaconScanInterval : 0 ))) seconds."
+echo -e "  > Est. to verify all ($numberOfOwners) owners as 'away' from all 'home': $(( numberOfOwners * (2 * verifyByRepeatedlyQuerying + verificationLoopDelay * verifyByRepeatedlyQuerying) + (beaconScanEnabled == 1 ? beaconScanInterval : 0 ))) seconds to $(( delayBetweenOwnerScansWhenPresent + numberOfOwners * nameScanTimeout * verificationLoopDelay * verifyByRepeatedlyQuerying + (beaconScanEnabled == 1 ? beaconScanInterval : 0 ))) seconds."
 
 #fuzz for one second per owner that is home, plus worst case 
-echo -e "  > Est. to recognize one owner is 'away': $(( 2 * verifyByRepeatedlyQuerying + verificationLoopDelay * verifyByRepeatedlyQuerying )) to $(( (beaconScanEnabled == 1 ? beaconScanInterval : 0 ) + delayBetweenOwnerScansWhenPresent + (numberOfOwners - 1) + nameScanTimeout * verificationLoopDelay * verifyByRepeatedlyQuerying )) seconds." 
+echo -e "  > Est. to verify one owner is 'away': $(( 2 * verifyByRepeatedlyQuerying + verificationLoopDelay * verifyByRepeatedlyQuerying )) to $(( (beaconScanEnabled == 1 ? beaconScanInterval : 0 ) + delayBetweenOwnerScansWhenPresent + (numberOfOwners - 1) + nameScanTimeout * verificationLoopDelay * verifyByRepeatedlyQuerying )) seconds." 
 
 #0.15 seconds is experimenatally obtained on a raspberry pi
 echo -e "  > Est. to recognize one owner is 'home': 0.15 seconds to $(( (beaconScanEnabled == 1 ? beaconScanInterval : 0 ) + delayBetweenOwnerScansWhenAway + (numberOfOwners - 1) + nameScanTimeout )) seconds." 
